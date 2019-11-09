af307cd9e1d32bc5d25fa4ff1ec4dd0ba522faac
Clark Williams <theclarkwilliams@gmail.com>
2019-11-08 18:01:23 -0700

    robot tests passgit add .!

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,65 +97,41 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-
-        # light = on
+        
         self.set_light_on()
-        # while light == on
+
         while self.light_is_on() == True:
-            # light = off
-            self.set_light_off()
-            # for x in list
+            
             for i in range(0, len(self._list)):
-                # if can move right: # account for end of list
-                if self.can_move_right() == True:
-                    # swap
-                    self.swap_item()
-                    # move right
-                    self.move_right()
+                for i in range(0, len(self._list)):
+                    if self.can_move_right() == True:
+                        self.swap_item()
+                        self.move_right()
                     
-                    # if compare <= 0  # item smaller than current position item, put smaller item back where it was
-                    if self.compare_item() <= 0:
-                        # move left # back to where none is stored
-                        self.move_left()
-                        # swap # replace none with smaller of values just compared
-                        self.swap_item()
-                        # move right
-                        self.move_right()
-                        # light = on
-                        self.set_light_on()
-                        # repeat line 101
-                    # elif: compare == 1: # if its bigger, swap and put smaller one in previous spot
+                        if self.compare_item() <= 0: # if smaller or equal, put it back
+                            self.move_left()
+                            self.swap_item()
+                            self.move_right()
+                        else:                    # if bigger, swap and put smaller one in previous spot
+                            self.swap_item()
+                            self.move_left()
+                            self.swap_item()
+                            self.move_right()
+
                     else:
-                        # x swap
-                        self.swap_item()
-                        # x move left
-                        self.move_left()
-                        # x swap 
-                        self.swap_item()
-                        # x move right
-                        self.move_right()
-                        # light = on
-                        self.set_light_on()
-                # else:
-                else:
-                    # while can move left
-                    while self.can_move_left() == True:
-                        # move left # move back to first item
-                        self.move_left()
-        # return list
+                        self.set_light_off()
+                        while self.can_move_left() == True:
+                            self.move_left()
+
         return self._list
-
-
-
-
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
    
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [10, 5, 8, 4, 6, 3, 7, 2, 9, 1]
     robot = SortingRobot(l)
 
     print(robot.sort())
-    print(robot._list)
+    # print(robot._list)
